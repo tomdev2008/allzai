92a2fe10cfa1a949cd731dcc4b372c2ee51631c5
huntun <huntunyuxuan@yeah.net>
2013-12-18 17:53:02 -0800

    allzai后台代码迁入

--- src/main/java/com/allzai/listener/StartupServlet.java
+++ src/main/java/com/allzai/listener/StartupServlet.java
@@ -1,10 +1,5 @@
 package com.allzai.listener;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-
 import javax.servlet.http.HttpServlet;
 
 public class StartupServlet extends HttpServlet {
@@ -15,23 +10,7 @@ public class StartupServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	{
-		//Process process = Runtime.getRuntime().exec(cmd);
-		
-	
-		
-	}
-	
-	public static void main(String[] args) throws IOException {
-		
-		try {
-			URL url = new URL("http://127.0.0.1:8080/");
-			URLConnection conn = url.openConnection();
-			conn.connect();
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+
 	}
 
 }
