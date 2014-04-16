f5b54df5d2fdd31dae3ca36b8bd1385212e9ce95
huntun <huntunyuxuan@yeah.net>
2014-04-16 14:14:58 +0800

    增加内部异常处理

--- server/allzai_server/src/main/java/com/allzai/server/isp/NetWorkServer.java
+++ server/allzai_server/src/main/java/com/allzai/server/isp/NetWorkServer.java
@@ -18,28 +18,41 @@ public class NetWorkServer {
 		
 		JsonObject json = new JsonObject();
 
-		if (StringUtil.isEmpty(form.getSip())) {
-			form.setSip(form.getIp());
+		try {
+			if (StringUtil.isEmpty(form.getSip())) {form.setSip(form.getIp());}
+			
+			/**
+			 * Hx0000:网络查询成功
+			 */
+			json.put("result", Boolean.TRUE);
+			json.put("code", "Hx0000");
+			
+			JsonObject object = new JsonObject();
+			IPLocation location = IPSeeker.getInstance().getIPLocation(form.getIp());
+			object.put("ip", form.getIp());
+			object.put("area", location.getArea());
+			object.put("country", location.getCountry());
+			json.put("ip", object);
+			
+			object = new JsonObject();
+			location = IPSeeker.getInstance().getIPLocation(form.getSip());
+			object.put("ip", form.getSip());
+			object.put("area", location.getArea());
+			object.put("country", location.getCountry());
+			json.put("sip", object);
+			
+			return json;
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+			/**
+			 *
+			 * Hx0001:查询内部异常
+			 */
+			json.put("result", Boolean.FALSE);
+			json.put("code", "Hx0001");
+			return json;
 		}
-		
-		json.put("result", Boolean.TRUE);
-		json.put("code", "Hx0000");
-		
-		JsonObject object = new JsonObject();
-		IPLocation location = IPSeeker.getInstance().getIPLocation(form.getIp());
-		object.put("ip", form.getIp());
-		object.put("area", location.getArea());
-		object.put("country", location.getCountry());
-		json.put("ip", object);
-		
-		object = new JsonObject();
-		location = IPSeeker.getInstance().getIPLocation(form.getSip());
-		object.put("ip", form.getSip());
-		object.put("area", location.getArea());
-		object.put("country", location.getCountry());
-		json.put("sip", object);
-		
-		return json;
 	}
 
 }
